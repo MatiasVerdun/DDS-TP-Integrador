5990a996d5af462e3339a757796608f2534f9c51
Matias <matiasverdun@hotmail.com>
2017-05-03 13:58:54 -0300

    Comienzo TP-Integrador

---
+++ WebTpIntegrador/src/java/TP_Integrador/DAO/UsuarioDAO.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP_Integrador.DAO;
+
+import TP_Integrador.DTO.Usuario;
+import TP_Integrador.MySQL.MySqlHelper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class UsuarioDAO {
+    
+    public boolean ValidarExistencia(Usuario usuario){
+        Boolean existe=false;
+        try {
+            //--- Se conecta a la base de datos
+            MySqlHelper mySQL = new MySqlHelper();
+            Connection conn = mySQL.getConnection();
+            
+            //--- Prepara la sentencia para validar el Usuario
+            PreparedStatement consultaUsuario = conn.prepareStatement("SELECT * FROM usuarios where id_usuario = ? AND contrasena=? ");
+            consultaUsuario.setString(1, usuario.getUserName());
+            consultaUsuario.setString(2, usuario.getPassword());
+
+            //--- Ejecuta la consulta
+            ResultSet rs = consultaUsuario.executeQuery();
+            //--- Verifica si pudo obtener al Usuuario
+            if(rs.next())   {
+                existe=true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al validar el usuario");
+        }
+        return existe;
+    }   
+    
+    public boolean ModificarClave(Usuario usuario, String strPasswordNew){
+        try {
+            
+            if (!this.ValidarExistencia(usuario))
+               return false;
+            
+            //--- Se conecta a la base de datos
+            MySqlHelper mySQL = new MySqlHelper();
+            Connection conn = mySQL.getConnection();
+            
+            //--- Prepara la sentencia para validar el Usuario
+            PreparedStatement consultaUsuario = conn.prepareStatement("UPDATE usuarios set contrasena=? where id_usuario = ? AND contrasena=? ");
+            consultaUsuario.setString(1, strPasswordNew);
+            consultaUsuario.setString(2, usuario.getUserName());
+            consultaUsuario.setString(3, usuario.getPassword());
+
+            //--- Ejecuta la consulta
+            consultaUsuario.execute();
+        } catch (SQLException ex) {
+            System.out.println("Error al modificar el usuario");
+        }
+        return true;
+    }   
+    
+}
---
+++ db/dds-tp-integrador.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-04-2017 a las 14:32:21
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dds-tp-integrador`
+--
+CREATE DATABASE IF NOT EXISTS `dds-tp-integrador` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `dds-tp-integrador`;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` varchar(50) NOT NULL,
+  `contrasena` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `contrasena`) VALUES
+('Hector', '1234'),
+
+
+--
+-- �?ndices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
