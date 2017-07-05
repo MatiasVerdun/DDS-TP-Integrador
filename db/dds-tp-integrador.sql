c02fedeb228baa855d45bc1f3850b060983b88d8
Matias <matiasverdun@hotmail.com>
2017-07-05 16:37:49 -0300

    Unificacion de base de datos

--- db/dds-tp-integrador.sql
+++ db/dds-tp-integrador.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-07-2017 a las 21:19:19
+-- Tiempo de generación: 05-07-2017 a las 21:34:52
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
